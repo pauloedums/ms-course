09f8987779a35eefef5b5b289a7fadaae4d19517
Paulo Marques <pauloedums@gmail.com>
2021-01-07 16:32:02 -0300

    user repository, userResource and zuul config

---
+++ hr-user/src/main/java/com/pauloedums/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.pauloedums.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pauloedums.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByEmail(String email);
+}
---
+++ hr-user/src/main/java/com/pauloedums/hruser/resources/UserResource.java
@@ -0,0 +1,34 @@
+package com.pauloedums.hruser.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pauloedums.hruser.entities.User;
+import com.pauloedums.hruser.repositories.UserRepository;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@Autowired
+	private UserRepository repository;
+		
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User worker = repository.findById(id).get();
+		return ResponseEntity.ok(worker);
+	}
+	
+
+	@GetMapping(value = "/search")
+	public ResponseEntity<User> findById(@RequestParam String email){
+		User worker = repository.findByEmail(email);
+		return ResponseEntity.ok(worker);
+	}
+}
